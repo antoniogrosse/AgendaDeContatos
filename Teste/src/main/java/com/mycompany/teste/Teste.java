32f588e231aa466bd0e0998ffff55a682039556d
antonio.sgrosse <antonio.sgrosse@CAS554860W0728.senacsp.edu.br>
2017-03-03 21:09:37 -0300

    Teste 2

--- Teste/src/main/java/com/mycompany/teste/Teste.java
+++ Teste/src/main/java/com/mycompany/teste/Teste.java
@@ -12,7 +12,7 @@ package com.mycompany.teste;
 public class Teste {
     
     public static void main(String[] args) {
-        System.out.println("aaaaa");
+        System.out.println("aaaagfga");
     }
     
 }
